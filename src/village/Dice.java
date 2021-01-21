850eef69c691175e61c8acb50ffdb7f467e98d32
Brian Tatman <btatman@lclark.edu>
2021-01-21 15:32:12 -0800

    Roll 6-sided die

--- src/village/Dice.java
+++ src/village/Dice.java
@@ -1,4 +1,10 @@
 package village;
 
+import java.util.Random;
+
 public class Dice {
+    public static int roll() {
+        Random random = new Random();
+        return random.nextInt(6) + 1;
+    }
 }
--- test/village/DiceTest.java
+++ test/village/DiceTest.java
@@ -1,7 +1,34 @@
 package village;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DiceTest {
+    @Test
+    void rollsOnlyValidSides() {
+        for (int i = 0; i < 10000; i++) {
+            int roll = Dice.roll();
+            assertTrue(roll >= 1 && roll <= 6);
+        }
+    }
 
+    @Test
+    void rollsAllSidesEventually() {
+        boolean[] sides = new boolean[7];
+        boolean allSidesRolled = false;
+        int times = 0;
+        while (!allSidesRolled) {
+            int roll = Dice.roll();
+            sides[roll] = true;
+            allSidesRolled = true;
+            for (int i = 1; i <= 6; i++) {
+                allSidesRolled = allSidesRolled && sides[i];
+            }
+            times++;
+            if (times == 1000000){
+                System.out.println("Your function is probably not working.");
+            }
+        }
+    }
 }
