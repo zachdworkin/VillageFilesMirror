6f56bc53d340e32835b5b5021fdcb82fc6383ca2
Brian Tatman <btatman@lclark.edu>
2021-01-26 14:34:05 -0800

    Added comments and ?s

--- src/village/Main.java
+++ src/village/Main.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Main {
     private Board board = new Board();
     private static char[] projects = {'#', 'H', '^', 'O', 'H', '^', 'O'};
+
     public static void main(String[] args) {
         Main main = new Main();
         main.takeTurn();
@@ -20,15 +21,16 @@ public class Main {
         System.out.println(board.toString());
         System.out.println("You get to place a " + projects[rolls[0]] + " in column " + rolls[1]);
         System.out.println("You get to place a " + projects[rolls[1]] + " in column " + rolls[0]);
-        System.out.println("In which row do want to build " + projects[rolls[0]]);
+        System.out.println("In which row do want to build " + projects[rolls[0]] + "?");
         Scanner input = new Scanner(System.in);
+        // User chooses which row to place project
         int row = input.nextInt();
-        Location project1 = new Location(row-1, rolls[1]-1);
+        Location project1 = new Location(row - 1, rolls[1] - 1);
         board.addProject(project1, projects[rolls[0]]);
         System.out.println(board.toString());
-        System.out.println("In which row do want to build " + projects[rolls[1]]);
+        System.out.println("In which row do want to build " + projects[rolls[1]] + "?");
         row = input.nextInt();
-        Location project2 = new Location(row-1, rolls[0]-1);
+        Location project2 = new Location(row - 1, rolls[0] - 1);
         board.addProject(project2, projects[rolls[1]]);
         System.out.println(board.toString());
     }
