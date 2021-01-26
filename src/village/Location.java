78c26ce0b087834b23c75391a38bbdafb284bc4c
Simon Buan <simonbuan@gmail.com>
2021-01-26 13:18:07 -0800

    Add location class
    Added constructor and getters for location class

--- src/village/Location.java
+++ src/village/Location.java
@@ -1,4 +1,22 @@
 package village;
 
 public class Location {
+    private int row;
+    private int col;
+
+    public Location(int row, int col) {
+        if(row >= 0  && row < 5) this.row = row;
+        else throw new ArrayIndexOutOfBoundsException();
+
+        if(col >= 0 && col < 6) this.col = col;
+        else throw new ArrayIndexOutOfBoundsException();
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getCol() {
+        return col;
+    }
 }
