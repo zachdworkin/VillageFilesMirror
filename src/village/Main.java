8461191cfd4c384f336d87fbf8961a332a0d39e2
zdworkin <zachdworkin@lclark.edu>
2021-01-28 18:51:21 -0800

    Bugfixes for InitialProjects
    Refactored Names
    Fixed input to only allow real inputs
    TODO add a method that creates an array list of valid rows a player can play in.

--- src/village/Main.java
+++ src/village/Main.java
@@ -1,6 +1,5 @@
 package village;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
@@ -12,7 +11,7 @@ public class Main {
 
     public static void main(String[] args) {
         Main main = new Main();
-        main.takePreTurn();
+        main.placeInitialProjects();
         while (true) {
             main.takeTurn();
         }
@@ -25,41 +24,42 @@ public class Main {
         System.out.println("Sum of rolls: " + rolls[2]);
     }
 
-    private void pickRowAndPlaceProject(int col) {
+    private void pickRowAndPlaceProject(int col, char proj) {
         System.out.println("In which row do want to build " + projects[col] + "?");
-        char row = checkInput(new ArrayList<Character>(Arrays.asList('1', '2', '3', '4', '5')));
+        //TODO add a helper function that creates the arraylist given to checkInput that checks if a row can take a project for a given column
+        String row = checkInput(new ArrayList<>(Arrays.asList("1", "2", "3", "4", "5")));
         //check if row, column is empty
-        System.out.println();
-        Location project1 = new Location((int)row - 1, rolls[1] - 1);
-        board.addProject(project1, projects[rolls[0]]);
+        System.out.println(Integer.parseInt(row));
+        Location project1 = new Location(Integer.parseInt(row) - 1, col - 1);
+        board.addProject(project1, proj);
         System.out.println(board.toString());
     }
 
     /**
      * helper function for checkInput
      * prints valid inputs
-     * @param validInputs
+     * @param selectableInputs an ArrayList of input strings to print
      */
-    private void printSelectables(ArrayList<Character> validInputs) {
+    private void printSelectables(ArrayList<String> selectableInputs) {
         System.out.print("You may select a: ");
-        for (char proj : validInputs) {
-            System.out.print(proj + " ");
+        for (String usrInpt : selectableInputs) {
+            System.out.print(usrInpt + " ");
         }
         System.out.println();
     }
 
     /**
      * Checks any user input for validity
-     * @param validInputs
+     * @param validInputs an ArrayList of valid input strings
      * @return their input as a character
      */
-    private char checkInput(ArrayList<Character> validInputs) {
+    private String checkInput(ArrayList<String> validInputs) {
         printSelectables(validInputs);
         Scanner input = new Scanner(System.in);
         while (true) {
-            char proj = input.next().charAt(0);
-            if (validInputs.contains(proj)) {
-                return proj;
+            String usrInpt = input.next();
+            if (validInputs.contains(usrInpt)) {
+                return usrInpt;
             } else{
                 System.out.println("Invalid input, please select an available input");
                 printSelectables(validInputs);
@@ -67,22 +67,23 @@ public class Main {
         }
     }
 
-    public void takePreTurn() {
-        ArrayList<Character> availablePreProjects = new ArrayList<Character>(Arrays.asList('H', '^', 'O'));
+    public void placeInitialProjects() {
+        ArrayList<String> availablePreProjects = new ArrayList<>(Arrays.asList("H", "^", "O"));
         getRolls();
         System.out.println("You start with two projects, select the first project to " +
                            "place in column " + rolls[0]);
 
-        char proj = checkInput(availablePreProjects);
+        String proj = checkInput(availablePreProjects);
         availablePreProjects.remove(availablePreProjects.indexOf(proj));
         System.out.println("You get to place a '" + proj + "' in column " + rolls[0] +
                            ". Select row to place it in.");
         System.out.println(board.toString());
 
-        pickRowAndPlaceProject(rolls[0]);
+        pickRowAndPlaceProject(rolls[0], proj.toCharArray()[0]);
 
         System.out.println("Select the second project to place in column " + rolls[1]);
         proj = checkInput(availablePreProjects);
+        pickRowAndPlaceProject(rolls[1], proj.toCharArray()[0]);
 
         //select projects to place (must be different cannot be square)
         //place first one
@@ -91,13 +92,37 @@ public class Main {
     }
 
     public void takeTurn() {
+        System.out.println("----------------------");
         getRolls();
         System.out.println(board.toString());
         //placeProject;
+//User chooses which row to place project
+//        int row = -1;
+//        Location location;
+//        // Make sure space is empty
+//        do {
+//            do {
+//                System.out.print("Enter a row (with an empty space) from 1 to 5: ");
+//                row = input.nextInt();
+//            } while (row < 1 || row > 5);
+//            location = new Location(row - 1, rolls[1] - 1);
+//        } while (!board.isEmpty(location));
+//
+//        board.addProject(location, projects[rolls[0]]);
+//        System.out.println(board.toString());
+//        System.out.println("In which row do want to build " + projects[rolls[1]] + "?");
+//        do {
+//            do {
+//                System.out.print("Enter a row (with an empty space) from 1 to 5: ");
+//                row = input.nextInt();
+//            } while (row < 1 || row > 5);
+//            location = new Location(row - 1, rolls[0] - 1);
+//        } while (!board.isEmpty(location));
+//        board.addProject(location, projects[rolls[1]]);
         System.out.println("You get to place a " + projects[rolls[0]] + " in column " + rolls[1]);
         System.out.println("You get to place a " + projects[rolls[1]] + " in column " + rolls[0]);
 
-        pickRowAndPlaceProject(rolls[0]);
-        pickRowAndPlaceProject(rolls[1]);
+        pickRowAndPlaceProject(rolls[0], projects[rolls[1]]);
+        pickRowAndPlaceProject(rolls[1], projects[rolls[0]]);
     }
 }
