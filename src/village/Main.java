96d736af5187804b28fbb003f90a15c1779529f5
Simon Buan <simonbuan@gmail.com>
2021-02-02 13:44:58 -0800

    Add bonus phase
    Minor refactoring of fields

--- src/village/Main.java
+++ src/village/Main.java
@@ -1,17 +1,35 @@
 package village;
 
 import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class Main {
-    private Board board = new Board();
     private static char[] projects = {'#', 'H', '^', 'O', 'H', '^', 'O'};
+
+    private Board board;
+    private Set<Character> bonusProjects;
+
+    public Main(){
+        bonusProjects = new TreeSet<>();
+        bonusProjects.add('^');
+        bonusProjects.add('H');
+        bonusProjects.add('O');
+
+        board = new Board();
+    }
 
     public static void main(String[] args) {
         Main main = new Main();
-        main.takeTurn();
+        for (int turn = 1; turn < 10; turn++) {
+            main.takeTurn(turn);
+        }
     }
 
-    public void takeTurn() {
+    public void takeTurn(int turn) {
+        if(turn % 3 == 0){
+            bonusPhase();
+        }
         // Roll the dice
         int[] rolls = Dice.rollTwice();
         boolean square = rolls[0] == rolls[1];
@@ -29,7 +47,7 @@ public class Main {
         System.out.println("In which row do want to build " + projects[rolls[0]] + "?");
         Scanner input = new Scanner(System.in);
         // User chooses which row to place project
-        int row = -1;
+        int row;
         Location location;
         // Make sure space is empty
         do {
@@ -66,5 +84,29 @@ public class Main {
             board.addProject(location, projects[rolls[1]]);
         }
         System.out.println(board.toString());
+    }
+
+    private void bonusPhase() {
+        System.out.println("Bonus phase. You get to draw an additional project in any (empty) space");
+        Scanner input = new Scanner(System.in);
+        char project;
+        do {
+            System.out.print("Enter what project you want to build: ");
+            project = input.next().charAt(0);
+        } while(!bonusProjects.contains(project));
+        bonusProjects.remove(project);
+
+        System.out.println("In which row and column do you want to build " + project + "?");
+        Location location;
+        int row, column;
+        do {
+            do {
+                System.out.print("Enter the row (from 1 to 5) and column (from 1 to 6): ");
+                row = input.nextInt();
+                column = input.nextInt();
+            } while (row < 1 || row > 5 || column < 1 || column > 6);
+            location = new Location(row - 1, column - 1);
+        } while (!board.isEmpty(location));
+        board.addProject(location, project);
     }
 }
